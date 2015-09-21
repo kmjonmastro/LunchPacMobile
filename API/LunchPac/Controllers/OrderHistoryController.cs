2579fbbb06eb92eb40e8bb0f1c3036e1f180d377
Mike Morrell <mmorrell@marathondata.com>
2015-09-21 17:23:49 -0400

    Added RestaurantId to OrderHistory GET.

--- API/LunchPac/Controllers/OrderHistoryController.cs
+++ API/LunchPac/Controllers/OrderHistoryController.cs
@@ -9,10 +9,11 @@ namespace LunchPac.Controllers
 {
     public class OrderHistoryController : ApiController
     {
-        public HttpResponseMessage Get(int userId)
+        public HttpResponseMessage Get(int userId, int restaurantId)
         {
             var orders = Repository<Order>
                 .SelectMany(o => o.UserId, userId)
+                .Where(o => o.RestaurantId == restaurantId)
                 .OrderByDescending(o => o.AddDate)
                 .GroupBy(o => new { o.OrderItem, o.OrderComments })
                 .Where(g => g.Count() == 1)
