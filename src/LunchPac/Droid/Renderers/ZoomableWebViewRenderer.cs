40bcae427ea8e02d81ee5ea9e9a0bcb12a997c77
Ryan McVeety <rmcveety@marathondata.com>
2015-09-22 15:45:17 -0400

    Zoomable WebView on Android

---
+++ src/LunchPac/Droid/Renderers/ZoomableWebViewRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Graphics.Drawables;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using LunchPac.Droid;
+using System.ComponentModel;
+
+[assembly:ExportRenderer(typeof(WebView), typeof(ZoomableWebViewRenderer))]
+
+namespace LunchPac.Droid
+{
+    public class ZoomableWebViewRenderer : WebViewRenderer
+    {
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (Control != null)
+            {
+                Control.Settings.BuiltInZoomControls = true;
+                Control.Settings.DisplayZoomControls = false;
+            }
+            base.OnElementPropertyChanged(sender, e);
+        }
+    }
+}
+
