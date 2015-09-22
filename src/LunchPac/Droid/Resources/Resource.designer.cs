422dea898b176cf1ce11bc3f2ef754a571955313
Ryan McVeety <rmcveety@marathondata.com>
2015-09-22 15:16:59 -0400

    Merge Cleanup

--- src/LunchPac/Droid/Resources/Resource.designer.cs
+++ src/LunchPac/Droid/Resources/Resource.designer.cs
@@ -112,13 +112,13 @@ namespace LunchPac.Droid
 			public const int logowhite = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int ww_icon = 2130837505;
+			public const int newcross = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int newcross = 2130837506;
+			public const int previous = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int previous = 2130837507;
+			public const int ww_icon = 2130837507;
 			
 			static Drawable()
 			{
